d4699fd6cd69299b125939d4cd00f5562e052580
vuasoclo <vuasoclo@gmail.com>
2024-12-06 11:56:49 +0700

    Step 1: create Java Spring demo

---
+++ src/main/java/trungbeso/entities/springbootsfademo/SpringBootsFaDemoApplication.java
@@ -0,0 +1,13 @@
+package trungbeso.entities.springbootsfademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootsFaDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootsFaDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/trungbeso/entities/springbootsfademo/SpringBootsFaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package trungbeso.entities.springbootsfademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootsFaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
